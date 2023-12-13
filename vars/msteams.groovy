8ba1f055e89773c0d2d911740bac4cf073b93633
Abdulla Donmez <36159762+AColtZz@users.noreply.github.com>
2023-12-13 11:17:33 +0100

    Update msteams.groovy

--- vars/msteams.groovy
+++ vars/msteams.groovy
@@ -1,4 +1,4 @@
-def upload(source, fileName, siteUrl, libraryName, credentialsId) {
+def upload(source, fileName, siteUrl, libraryName, clientID, clientSecret) {
     bat(label: "Upload files to Teams", script: """
         SET "AccessToken="
         FOR /f "tokens=1,2 delims=:, " %%U in ('
