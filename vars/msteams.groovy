a969d419075a489268499850d9b07ef1c8177975
Abdulla Donmez <36159762+AColtZz@users.noreply.github.com>
2023-12-13 10:58:06 +0100

    Update msteams.groovy

--- vars/msteams.groovy
+++ vars/msteams.groovy
@@ -1,37 +1,19 @@
-@Library('JenkinsSharedLib@master') _
-
-import groovyx.net.http.RESTClient
-import groovy.util.Base64
-
 def uploadToTeams(source, fileName, siteUrl, libraryName, credentialsId) {
-    // Load HTTP Builder classes
-    def httpBuilderClass = this.getClass().classLoader.loadClass('groovyx.net.http.RESTClient')
-    def base64Class = this.getClass().classLoader.loadClass('groovy.util.Base64')
+    bat(label: "Upload files to Teams", script: """
+        SET "AccessToken="
+        FOR /f "tokens=1,2 delims=:, " %%U in ('
+            curl ^
+            --request POST ^
+            --data "client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token" ^
+            https://accounts.google.com/o/oauth2/token ^| findstr /i "\"access_token\""
+        ') DO SET "AccessToken=,%%~V"
+        IF DEFINED AccessToken(SET "AccessToken=%AccessToken:~1%") ELSE (SET "AccessToken=n/a")
+        SET AccessToken
 
-    def client = httpBuilderClass.newInstance()
-    
-    withCredentials([string(credentialsId: credentialsId, variable: 'credentials')]) {
-        def auth = "${credentials}".getBytes(base64Class).encodeBase64().toString()
-
-        def fileContent = new File(source).bytes
-
-        def response = client.post(
-            path: "/_api/web/lists/getbytitle('${libraryName}')/RootFolder/Files/Add(url='${fileName}.zip',overwrite=true)",
-            headers: [
-                'Authorization': "Basic ${auth}",
-                'Accept': 'application/json;odata=verbose',
-                'X-RequestDigest': 'FormDigestValue'
-            ],
-            body: [
-                "length": fileContent.length,
-                "content": fileContent
-            ]
-        )
-
-        if (response.status == 200) {
-            println "File uploaded successfully."
-        } else {
-            println "Error uploading file: ${response.status} - ${response.data}"
-        }
-    }
+        curl -X POST ^
+        -H "Authorization: Bearer %AccessToken%" ^
+        -F "metadata={ mimeType: 'application/x-zip-compressed', kind: 'drive#file', name: '${fileName}.zip', parents: ['${parents}'] };type=application/json;charset=UTF-8" ^
+        -F "file=@${source};type=application/x-zip-compressed" ^
+        "https://${siteUrl}/_api/web/lists/getbytitle('${libraryName}')/RootFolder/Files/Add(url='${fileName}.zip',overwrite=true)"
+    """)
 }
