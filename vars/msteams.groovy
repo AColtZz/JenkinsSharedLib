83509209e252a4e753cb1b7b6e724c0eb5db9e3d
Abdulla Donmez <36159762+AColtZz@users.noreply.github.com>
2023-12-13 11:15:23 +0100

    Update msteams.groovy

--- vars/msteams.groovy
+++ vars/msteams.groovy
@@ -4,15 +4,15 @@ def uploadToTeams(source, fileName, siteUrl, libraryName, credentialsId) {
         FOR /f "tokens=1,2 delims=:, " %%U in ('
             curl ^
             --request POST ^
-            --data "client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token" ^
-            https://accounts.google.com/o/oauth2/token ^| findstr /i "\"access_token\""
+            --data "grant_type=client_credentials&client_id=${clientID}&client_secret=${clientSecret}&resource=https%3A%2F%2Fyourtenant.sharepoint.com%2F" ^
+            https://login.microsoftonline.com/yourtenant/oauth2/token ^| findstr /i "\"access_token\""
         ') DO SET "AccessToken=,%%~V"
-        IF DEFINED AccessToken(SET "AccessToken=%AccessToken:~1%") ELSE (SET "AccessToken=n/a")
+        IF DEFINED AccessToken (SET "AccessToken=%AccessToken:~1%") ELSE (SET "AccessToken=n/a")
         SET AccessToken
 
         curl -X POST ^
         -H "Authorization: Bearer %AccessToken%" ^
-        -F "metadata={ mimeType: 'application/x-zip-compressed', kind: 'drive#file', name: '${fileName}.zip', parents: ['${parents}'] };type=application/json;charset=UTF-8" ^
+        -H "Accept: application/json;odata=verbose" ^
         -F "file=@${source};type=application/x-zip-compressed" ^
         "https://${siteUrl}/_api/web/lists/getbytitle('${libraryName}')/RootFolder/Files/Add(url='${fileName}.zip',overwrite=true)"
     """)
