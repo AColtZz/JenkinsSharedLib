4bf6f7f8ee94d0e7cc59319a8f0e30423e3be4db
Abdulla Donmez <36159762+AColtZz@users.noreply.github.com>
2024-02-28 15:19:37 +0100

    Update msteams.groovy

--- vars/msteams.groovy
+++ vars/msteams.groovy
@@ -1,4 +1,4 @@
-def upload(auth_file, file_path, file_name, upload_folder) 
+def upload(script, auth_file, file_path, file_name, upload_folder) 
 {
-    bat(label: "running ms_upload.py", script: "python ../scripts/ms_upload.py \"${auth_file}\" \"${file_path}\" \"${file_name}\" \"${upload_folder}\"")
+    bat(label: "running ${script}", script: "python ${script} \"${auth_file}\" \"${file_path}\" \"${file_name}\" \"${upload_folder}\"")
 }
