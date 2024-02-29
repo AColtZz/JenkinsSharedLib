d4fe9a16861c28198f7e3095c4bad3d64f238ccb
Abdulla Donmez <36159762+AColtZz@users.noreply.github.com>
2024-02-29 11:51:18 +0100

    Update msteams.groovy

--- vars/msteams.groovy
+++ vars/msteams.groovy
@@ -1,10 +1,17 @@
 def upload(script, auth_file, file_path, file_name, upload_folder) 
 {
-    bat(label: "running ${script}", script: "python ${script} \"${auth_file}\" \"${file_path}\" \"${file_name}\" \"${upload_folder}\"")
+    def output = bat(label: "running ${script}", script: "python ${script} \"${auth_file}\" \"${file_path}\" \"${file_name}\" \"${upload_folder}\"")
     
     // Check if the script succeeded
     if (output == 0) {
         // Extract the URL from the output and set it to the environment variable
-        env.UPLOAD_URL = output.substring(output.indexOf("URL:") + 5).trim()
+        def urlStartIndex = output.indexOf("URL:")
+        if (urlStartIndex != -1) {
+            env.UPLOAD_URL = output.substring(urlStartIndex + 5).trim()
+            echo "File uploaded successfully. URL: ${env.UPLOAD_URL}"
+        } else {
+            echo "Failed to extract URL from the output: ${output}"
+            // Handle failure if needed
+        }
     }
 }
