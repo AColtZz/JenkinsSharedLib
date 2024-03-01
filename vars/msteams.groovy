fd70361c2b75f19caa95cd10badff85e674f3fb5
Abdulla Donmez <36159762+AColtZz@users.noreply.github.com>
2024-03-01 13:26:01 +0100

    Update msteams.groovy

--- vars/msteams.groovy
+++ vars/msteams.groovy
@@ -5,13 +5,7 @@ def upload(script, auth_file, file_path, file_name, upload_folder)
     // Check if the script succeeded
     if (output == 0) {
         // Extract the URL from the output and set it to the environment variable
-        def urlStartIndex = output.indexOf("URL:")
-        if (urlStartIndex != -1) {
-            env.UPLOAD_URL = output.substring(urlStartIndex + 5).trim()
-            echo "File uploaded successfully. URL: ${env.UPLOAD_URL}"
-        } else {
-            echo "Failed to extract URL from the output: ${output}"
-            // Handle failure if needed
-        }
+        env.UPLOAD_URL = output.substring(output.indexOf("URL:")).trim()
+        echo "File uploaded successfully. URL: ${env.UPLOAD_URL}"
     }
 }
