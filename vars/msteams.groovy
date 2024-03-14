15648eb375a3fe9d55fe32616a472c642ccc7e05
Abdulla Donmez <36159762+AColtZz@users.noreply.github.com>
2024-03-14 13:35:50 +0100

    Update msteams.groovy

--- vars/msteams.groovy
+++ vars/msteams.groovy
@@ -1,16 +1,16 @@
 def upload(script, auth_file, file_path, file_name, upload_folder) 
 {
-    // def output = bat(label: "running ${script}", script: "python ${script} \"${auth_file}\" \"${file_path}\" \"${file_name}\" \"${upload_folder}\"")
-    def output = bat(
-        label: "running ${script}",
-        script: "python ${script} \"${auth_file}\" \"${file_path}\" \"${file_name}\" \"${upload_folder}\"",
-        returnStatus: true,  // Capture the exit code
-        returnStdout: true   // Capture the output
-    ).trim()
+    bat(label: "running ${script}", script: "python ${script} \"${auth_file}\" \"${file_path}\" \"${file_name}\" \"${upload_folder}\"")
+    // def output = bat(
+    //     label: "running ${script}",
+    //     script: "python ${script} \"${auth_file}\" \"${file_path}\" \"${file_name}\" \"${upload_folder}\"",
+    //     returnStatus: true,  // Capture the exit code
+    //     returnStdout: true   // Capture the output
+    // ).trim()
 
-    // Check if the script succeeded
-    if (output == 0) {
-        // Extract the URL from the output and set it to the environment variable
-        env.UPLOAD_URL = output.substring(output.indexOf("URL:")).trim()
-    }
+    // // Check if the script succeeded
+    // if (output == 0) {
+    //     // Extract the URL from the output and set it to the environment variable
+    //     env.UPLOAD_URL = output.substring(output.indexOf("URL:")).trim()
+    // }
 }
